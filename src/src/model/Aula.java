078a570f4e68991487cb852774dc1e6bfd113fd4
Rafael Trajano <rafatrajaninhortn@gmail.com>
2026-03-18 10:31:21 -0300

    Código Java com o tempo de execução (src)
    Código Java com o tempo de execução (src)

---
+++ src/src/model/Aula.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Aula {
+
+    private int inicio;
+    private int fim;
+
+    public Aula(int inicio, int fim) {
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public int getInicio() {
+        return inicio;
+    }
+
+    public int getFim() {
+        return fim;
+    }
+}
